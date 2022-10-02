25993a6bd51514f541fb2536e1cf41fdf7b056a6
JKAX2022 <junedk@axioned.com>
2022-10-02 15:36:29 +0530

    JS Compelted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-/* Author: Zishan Ansari */
+var counters = document.querySelectorAll(".counter");
+
+// Fucntion for each Counter
+counters.forEach(function (counter) {
+  counter.innerText="0";
+
+// function for updating counter number
+  function counterUpdate() {
+    var counterTraget = counter.getAttribute('data-target');
+    var number = +counter.innerText;
+    var increment = counterTraget / 250;
+
+    if (number < counterTraget) {
+      counter.innerText = `${Math.ceil(number + increment)}`;
+      setTimeout(counterUpdate, 1);
+    }
+  };
+  counterUpdate();
+});
