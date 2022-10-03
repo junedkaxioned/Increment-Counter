d64459f5a830715dd467cef094ccf4f5241050e7
junedkaxioned <junedk@axioned.com>
2022-10-03 15:44:01 +0530

    JS full code changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
 var counters = document.querySelectorAll(".counter");
 
-// Fucntion for each Counter
-counters.forEach(function (counter) {
-  counter.innerText="0";
+// FUnction for each counter
+counters.forEach(function (abc) {
+  var counterTraget = +(abc.getAttribute('data-target'));
+  var initial = counterTraget / 100;
+  var x = 1;
 
-// function for updating counter number
-  function counterUpdate() {
-    var counterTraget = counter.getAttribute('data-target');
-    var number = +counter.innerText;
-    var increment = counterTraget / 250;
-
-    if (number < counterTraget) {
-      counter.innerText = `${Math.ceil(number + increment)}`;
-      setTimeout(counterUpdate, 1);
+  // Setnterval function 
+  var interval = setInterval(function () {
+    abc.innerText = initial * x;
+    x++;
+    if (x > 100) {
+      clearInterval(interval);
     }
-  };
-  counterUpdate();
-});
+  }, 20)
+
+})
